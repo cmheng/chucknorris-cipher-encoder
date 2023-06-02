9ad0f4d11d0c7dce64f19a3360d9665e9d758402
cmheng <cmheng@outlook.sg>
2023-06-02 11:58:25 +0800

    Completed Stage 2

--- src/chucknorris/Main.java
+++ src/chucknorris/Main.java
@@ -9,11 +9,11 @@ public class Main {
         String input = scanner.nextLine();
 
         System.out.println();
-        StringBuilder encrypted = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            encrypted.append(input.charAt(i));
-            if (i != input.length() - 1) encrypted.append(" ");
+        System.out.println("The result:");
+        for (char c : input.toCharArray()) {
+            String binary = Integer.toBinaryString(c);
+            int intBinary = Integer.parseInt(binary);
+            System.out.printf("%s = %07d\n", c, intBinary);
         }
-        System.out.println(encrypted);
     }
 }
